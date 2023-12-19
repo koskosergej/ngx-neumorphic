bbbafec7061e80150ece8215f98b22d5679412c8
Siarhei Kasko <siarhei.kasko@instinctools.com>
2023-12-19 14:34:31 +0100

    Add memo pipe

---
+++ src/pipes/memo/memo.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'memo'
+})
+export class MemoPipe<T, K, Z, Q> implements PipeTransform {
+  transform(value: T, handler: (value: T, ...args: K) => Z, context?: Q, ...args: Array<K>): Z {
+    if (context && handler) {
+      return handler.apply(context, [value, ...args]);
+    }
+
+    return handler(value, ...args);
+  }
+}
